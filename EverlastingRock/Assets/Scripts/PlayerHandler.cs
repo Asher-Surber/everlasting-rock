e7deac7aed1306dcf4ac62a783021452a249d729
Asher-Surber <ashersurber@gmail.com>
2021-10-04 12:43:17 -0400

    Implemented HP, UI updates

--- EverlastingRock/Assets/Scripts/PlayerHandler.cs
+++ EverlastingRock/Assets/Scripts/PlayerHandler.cs
@@ -2,18 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class PlayerHandler : MonoBehaviour
 {
     public GameObject haveKeyText;
-    //public bool canWin;
     public GameObject winZone;
+    public TextMeshProUGUI hpText;
+    public GameObject findKeyText;
+    private int hp = 3;
 
     // Start is called before the first frame update
     void Start()
     {
         haveKeyText.SetActive(false);
-        //canWin = false;
+        findKeyText.SetActive(true);
         winZone.SetActive(false);
         
     }
@@ -28,23 +31,28 @@ public class PlayerHandler : MonoBehaviour
     {
         
         if (other.CompareTag("Angel")) {
-            SceneManager.LoadSceneAsync("Assets/Scenes/GameOver.unity");
-            Scene gameOver = SceneManager.GetSceneByPath("Assets/Scenes/GameOver.unity");
-            SceneManager.SetActiveScene(gameOver);
+            hp -= 1;
+            hpText.text = "HP: " + hp;
+            if (hp == 0)
+            {
+                SceneManager.LoadScene("Assets/Scenes/GameOver.unity");
+                Scene gameOver = SceneManager.GetSceneByPath("Assets/Scenes/GameOver.unity");
+                SceneManager.SetActiveScene(gameOver);
+            }
 
         }
 
         if (other.CompareTag("Key"))
         {
             other.gameObject.SetActive(false);
+            findKeyText.SetActive(false);
             haveKeyText.gameObject.SetActive(true);
-            //canWin = true;
             winZone.SetActive(true);
         }
 
         if (other.CompareTag("WinZone"))
         {
-            SceneManager.LoadSceneAsync("Assets/Scenes/Win.unity");
+            SceneManager.LoadScene("Assets/Scenes/Win.unity");
             Scene win = SceneManager.GetSceneByPath("Assets/Scenes/Win.unity");
             SceneManager.SetActiveScene(win);
         }
